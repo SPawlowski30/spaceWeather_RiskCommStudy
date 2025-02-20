d7080bbac7bd5dc5042b62338b5c8fd79615ae12
Elizabeth Pauley <elizpauley@gmail.com>
2025-02-19 23:56:36 -0700

    Renamed some files, uploaded a pdf, initial preprocessing code
    - Removed spaces from the names of the email alert text files
    - Uploaded an academic article in order to test the PDF reader
    - Initial code successfully extracts text from one text file, one PDF, and one news article

---
+++ 02_Code/01_Preprocessing/preprocessing.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+from pypdf import PdfReader
+import requests
+
+# EMAIL ALERTS
+# update to look at all alerts from past 5 years...if that's possible?
+forecastDiscussion = open('../../01_Data/Email_Alerts/ForecastDiscussion.txt', "r")
+forecastDiscussionContent = forecastDiscussion.read()
+print(forecastDiscussionContent)
+
+# ACADEMIC ARTICLES
+futureSpaceWeatherOpsResearch = PdfReader('../../01_Data/Academic_Articles/PlanningFutureSpaceWeatherOpsAndResearch.pdf')
+for page in range(7, 108): # 7-108 are the actual desired pages; this excludes everything before preface and appendixes
+    currPage = futureSpaceWeatherOpsResearch.pages[page]
+    pageText = currPage.extract_text()
+    print(pageText)
+
+# NEWS ARTICLES
+mitUrl = 'https://news.mit.edu/2013/space-weather-effects-on-satellites-0917'
+mitArticle = requests.get(mitUrl)
+mitArticleSoup = BeautifulSoup(mitArticle.text, 'html.parser')
+
+# collect text only from the div that contains the body of the article; strip of HTML
+for mitData in mitArticleSoup.find_all('div',{'class':'paragraph'}):
+    print(mitData.text.strip())
+    # TO-DO: split data into sentences, append each sentence to dataframe
+
+# NUMERICAL DATA (MAYBE CREATE A SEPARATE FILE FOR THIS?)
+
