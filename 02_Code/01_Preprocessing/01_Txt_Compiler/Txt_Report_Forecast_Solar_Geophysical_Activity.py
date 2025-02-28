df99c7ef35dfd24c8285ec59d281613dee47be52
SPawlowski30 <sarahlpawlowski@gmail.com>
2025-02-27 21:37:00 -0700

    added custom cleaning to Report_Forecast

--- 02_Code/01_Preprocessing/01_Txt_Compiler/Txt_Report_Forecast_Solar_Geophysical_Activity.py
+++ 02_Code/01_Preprocessing/01_Txt_Compiler/Txt_Report_Forecast_Solar_Geophysical_Activity.py
@@ -9,6 +9,20 @@ df = pd.read_csv(csv_path)
 # Output file to store all the combined text
 output_filename = "Txt_Report_Forecast_Solar_Geophysical_Activity.txt"
 
+def remove_III(text):
+    lines = text.splitlines()
+    new_lines = []
+    found_III = False
+
+    for line in lines:
+        if found_III:
+            break
+        if line.startswith("III."):
+            found_III = True
+            continue
+        new_lines.append(line)
+    return"\n".join(new_lines)
+
 # Open the output file in write mode
 with open(output_filename, "w", encoding="utf-8") as outfile:
     for index, row in df.iterrows():
@@ -17,9 +31,12 @@ with open(output_filename, "w", encoding="utf-8") as outfile:
         response = requests.get(file_url)
         response.raise_for_status()  # stops if an error occurs
 
+        lines = response.text.splitlines()
+        removed_6 = "\n".join(lines[6:])
+        cleaned_text = remove_III(removed_6)
+
         # Write content to the output file
-        outfile.write(response.text)
-        outfile.write("\n\n")  # Separate files with blank lines
+        outfile.write(cleaned_text +"\n\n")  # Separate files with blank lines
 
 
 print(f"All files combined into {output_filename}")
