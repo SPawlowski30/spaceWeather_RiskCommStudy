63c675bbacc295a1f0730065ab24aba4e97f1574
SPawlowski30 <sarahlpawlowski@gmail.com>
2025-02-27 21:07:13 -0700

    adding custom scrubbing to txt compiler file

--- 02_Code/01_Preprocessing/01_Txt_Compiler/Txt_3_Day_Forecast.py
+++ 02_Code/01_Preprocessing/01_Txt_Compiler/Txt_3_Day_Forecast.py
@@ -9,6 +9,19 @@ df = pd.read_csv(csv_path)
 # Output file to store all the combined text
 output_filename = "Txt_3_Day_Forecast.txt"
 
+def is_tab(line):
+    words = line.split()
+    num_count = sum (1 for word in words if word.replace(".", "",1).isdigit())
+    space_count = line.count("  ")
+    return num_count > len(words)/2 or space_count >=2
+
+def scrub_a_dub(line):
+    unwanted_char = ("A.", "B.", "C.", "Rationale:", ":Product:", ":Issued:")
+    for prefix in unwanted_char:
+        if line.startswith(prefix):
+            return line[len(prefix):].strip()
+    return line
+
 # Open the output file in write mode
 with open(output_filename, "w", encoding="utf-8") as outfile:
     for index, row in df.iterrows():
@@ -16,10 +29,10 @@ with open(output_filename, "w", encoding="utf-8") as outfile:
 
         response = requests.get(file_url)
         response.raise_for_status()  # stops if an error occurs
-
+        text_data = response.text.splitlines()
+        cleaned_text_data = [scrub_a_dub(line) for line in text_data if not is_tab(line)]
         # Write content to the output file
-        outfile.write(response.text)
-        outfile.write("\n\n")  # Separate files with blank lines
-
+        outfile.write("\n".join(cleaned_text_data) + "\n\n")  # Separate files with blank lines
+        print(f"Finished processing file {index + 1}")
 
 print(f"All files combined into {output_filename}")
